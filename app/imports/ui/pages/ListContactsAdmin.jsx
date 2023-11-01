dd028e4c4aa841a95badc5a199c3b18e605c4875
Silvia <silviadb48@gmail.com>
2023-10-31 16:03:07 -1000

    contacts collection finished in 28 minutes.

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -3,17 +3,17 @@ import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import LoadingSpinner from '../components/LoadingSpinner';
-import Contact from '../components/Contact';
 import { Contacts } from '../../api/contact/Contacts';
+import ContactAdmin from '../components/ContactAdmin';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-const ListContacts = () => {
+const ListContactsAdmin = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { ready, contacts } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Contacts.userPublicationName);
+    const subscription = Meteor.subscribe(Contacts.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
@@ -23,16 +23,15 @@ const ListContacts = () => {
       ready: rdy,
     };
   }, []);
-
   return (ready ? (
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col>
           <Col className="text-center">
-            <h2>List Contacts</h2>
+            <h2>List Contacts (Admin)</h2>
           </Col>
           <Row xs={1} md={2} lg={3} className="g-4">
-            {contacts.map((contact) => (<Col key={contact.id}><Contact contact={contact} /></Col>))}
+            {contacts.map((contact) => (<Col key={contact.id}><ContactAdmin contact={contact} /></Col>))}
           </Row>
         </Col>
       </Row>
@@ -40,4 +39,4 @@ const ListContacts = () => {
   ) : <LoadingSpinner />);
 };
 
-export default ListContacts;
+export default ListContactsAdmin;
